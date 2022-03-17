0cc25f5a220c8695ab3ed70b9619c0c0b1c7bf86
HarshvardhanShringi <81759722+HarshvardhanShringi@users.noreply.github.com>
2022-03-17 09:09:04 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,9 @@
 from cryptography.fernet import Fernet
 key=Fernet.generate_key()
+data='hey welcome'
 token=Fernet(key)
-encrypted_text=token.encrypt(b"happy new year")
-#print(cipher_text)
-#print(key)
+encrypted_text=token.encrypt(data.encode())
 decrypted_text=token.decrypt(encrypted_text)
+print(encrypted_text)
+print(key)
 print(decrypted_text)
