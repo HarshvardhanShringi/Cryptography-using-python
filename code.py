f49838d8a833ae820adefa16dec1a93bfd6664d7
HarshvardhanShringi <81759722+HarshvardhanShringi@users.noreply.github.com>
2022-03-17 13:15:18 +0530

    Delete code.py

--- code.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-key=Fernet.generate_key()
-data='hey welcome'
-token=Fernet(key)
-encrypted_text=token.encrypt(data.encode())
-decrypted_text=token.decrypt(encrypted_text)
-print(encrypted_text)
-print(key)
-print(decrypted_text)
